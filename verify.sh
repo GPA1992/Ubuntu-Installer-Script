616098a88731014a1aef9fd92df517f2a1bb4b11
GPA <gabrielproencaaraujo@gmail.com>
2023-04-17 17:00:49 -0300

    Verify description update

--- verify.sh
+++ verify.sh
@@ -2,78 +2,100 @@
 
 # Verifica se o gdebi-core está instalado
 if [ $(dpkg-query -W -f='${Status}' gdebi-core 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-    echo "gdebi-core: ✔️ $(dpkg-query -W -f='${Version}' gdebi-core)"
+    echo "GDEBI-CORE: ✔️ $(dpkg-query -W -f='${Version}' gdebi-core)"
 else
-    echo "gdebi-core: ❌ não instalado"
+    echo "GDEBI-CORE: ❌ não instalado"
 fi
 
 # Verifica se o Zoom está instalado
 if [ $(dpkg-query -W -f='${Status}' zoom 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-    echo "Zoom: ✔️ $(dpkg-query -W -f='${Version}' zoom)"
+    echo "ZOOM: ✔️ $(dpkg-query -W -f='${Version}' zoom)"
 else
-    echo "Zoom: ❌ não instalado"
+    echo "ZOOM: ❌ não instalado"
 fi
 
 # Verifica se o snapd está instalado
 if [ -x "$(command -v snap)" ]; then
-    echo "snapd: ✔️ $(snap version | awk '/snapd/ {print $2}')"
+    echo "SNAP: ✔️ $(snap version | awk '/snapd/ {print $2}')"
 else
-    echo "snapd: ❌ não instalado"
+    echo "SNAP: ❌ não instalado"
 fi
 
 # Verifica se o Slack está instalado
 if [ -x "$(command -v slack)" ]; then
-    echo "Slack: ✔️  Na versão mais recente"
+    echo "SLACK: ✔️  Na versão mais recente"
 else
-    echo "Slack: ❌ não instalado"
+    echo "SLACK: ❌ não instalado"
 fi
 
 # Verifica se o Visual Studio Code está instalado
 if [ $(dpkg-query -W -f='${Status}' code 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-    echo "Visual Studio Code: ✔️ $(dpkg-query -W -f='${Version}' code)"
+    echo "VSCODE: ✔️ $(dpkg-query -W -f='${Version}' code)"
 else
-    echo "Visual Studio Code: ❌ não instalado"
+    echo "VSCODE: ❌ não instalado"
 fi
 
 # Verifica se o Google Chrome está instalado
 if [ $(dpkg-query -W -f='${Status}' google-chrome-stable 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-    echo "Google Chrome: ✔️ $(dpkg-query -W -f='${Version}' google-chrome-stable)"
+    echo "GOOGLE CHROME: ✔️ $(dpkg-query -W -f='${Version}' google-chrome-stable)"
 else
-    echo "Google Chrome: ❌ não instalado"
+    echo "GOOGLE CHROME: ❌ não instalado"
 fi
 
 # Verifica se o Discord está instalado
 if [ $(dpkg-query -W -f='${Status}' discord 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-    echo "Discord: ✔️ $(dpkg-query -W -f='${Version}' discord)"
+    echo "DISCORD: ✔️ $(dpkg-query -W -f='${Version}' discord)"
 else
-    echo "Discord: ❌ não instalado"
+    echo "DISCORD: ❌ não instalado"
 fi
 
 # Verifica se o Node.js está instalado
 if [ -x "$(command -v node)" ]; then
-    echo "Node.js: ✔️ $(node -v)"
+    echo "NODE.JS: ✔️ $(node -v)"
 else
-    echo "Node.js: ❌ não instalado"
+    echo "NODE.JS: ❌ não instalado"
 fi
 
 # Verifica se o programa Docker está instalado
 if [ -x "$(command -v docker)" ]; then
-    echo "Docker: ✔️ $(docker --version | awk '{print $3}')"
+    echo "DOCKER: ✔️ $(docker --version | awk '{print $3}')"
 else
-    echo "Docker: ❌ não instalado"
+    echo "DOCKER: ❌ não instalado"
 fi
 
 # Verifica se o programa Docker Compose está instalado
 if [ -x "$(command -v docker-compose)" ]; then
-    echo "Docker Compose: ✔️ $(docker-compose --version | awk '{print $3}')"
+    echo "DOCKER COMPOSE: ✔️ $(docker-compose --version | awk '{print $3}')"
 else
-    echo "Docker Compose: ❌ não instalado"
+    echo "DOCKER COMPOSE: ❌ não instalado"
 fi
 
 
 # Verifica se o venv esta instalado
 if [ -x "$(command -v python3 -m venv)" ]; then
-    echo "Venv: ✔️ $(dpkg-query --show --showformat='${Version}\n' python3-venv)"      
+    echo "VENV: ✔️ $(dpkg-query --show --showformat='${Version}\n' python3-venv)"      
 else
-    echo "Venv: ❌ não instalado"
+    echo "VENV: ❌ não instalado"
+fi
+
+#verifica se o Spotify esta instalado
+if [ -x "$(command -v spotify)" ]; then
+   echo "Spotify: ✔️ $(dpkg -l spotify-client | grep spotify-client | awk '{print $3}')" 
+else
+    echo "SPOTIFY: ❌ não instalado"
+fi
+
+
+#verifica se o CURL esta instalado
+if [ -x "$(command -v curl)" ]; then
+    echo "CURL: ✔️ $(curl --version | awk 'NR==1{print $2}')"
+else
+    echo "CURL: ❌ não instalado"
+fi
+
+# Verificar se o git já está instalado
+if [ -x "$(command -v git)" ]; then
+  echo "GIT: ✔️ $(git --version)"
+else
+  echo "GIT: ❌ não instalado"
 fi
